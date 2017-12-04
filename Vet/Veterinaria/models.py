8927904c7a6dd79d717235eb1700cb7d265b7f6b
Luis Sanchez G <luis.sanchezgu@usach.cl>
2017-12-03 21:58:11 -0300

    Añadiendo atributos al dueño y conectando mascotas a dueños

--- Vet/Veterinaria/models.py
+++ Vet/Veterinaria/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.urls import reverse_lazy
 
+class Duegno(models.Model):
+    class Meta:
+        verbose_name = u'Dueño'
+    first_name = models.CharField('Nombre', max_length=40)
+    last_name = models.CharField('Apellido', max_length=40)
+    age = models.IntegerField()
+    email = models.EmailField(null=True, blank=True)
+    phone = models.CharField(null=True, blank=True, max_length=10)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    def __unicode__(self):
+        return self.first_name + " " + self.last_name
 
 # TODO crear formulario público para crear mascotas
 # TODO asignar URL de objeto para que al crearlo nos vayamos allá mismo
@@ -12,21 +26,17 @@ class Mascota(models.Model):
 
     name = models.CharField('Nombre', max_length=10)
     date_of_birth = models.DateField('Fecha de nacimiento')
+    duegno = models.ForeignKey(Duegno, on_delete=models.CASCADE, null=True) # https://docs.djangoproject.com/en/1.11/ref/models/fields/#django.db.models.ForeignKey.on_delete
 
     def get_absolute_url(self):
         return reverse_lazy('detail_pet', kwargs={'pk': self.id})
 
     def __str__(self):
-        return "Mascota de nombre: " + self.name
+        return self.name
 
     def __unicode__(self):
         return self.name
 
 
-class Duegno(models.Model):
-    class Meta:
-        verbose_name = u'Dueño'
-    name = models.CharField('Dueño', max_length=40)
-    age = models.IntegerField()
 
 
